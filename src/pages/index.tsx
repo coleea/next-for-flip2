aa18565f9fa04cff8b5bb1f2485eefa431985513
LeeKB <tmwlak@gmail.com>
2023-09-17 18:19:18 +0900

    _

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,74 +62,78 @@ export default function MyBook({}) {
                 </h1>
               </button>
             </div>}
-        {isFullscreen &&
-        
-          <div ref={ref} className="flex justify-center" style={{overflow : "hidden", background : "black"}}>      
-              {width && height&& (
-                <HTMLFlipBook
-                    onFlip={(e) => console.log("flip", e)}
-                    onUpdate={(e) => console.log("update", e)}
-                    onChangeState={(e) => {
-                      console.log("changeState", e)
-                      if(ref.current) {
-                        console.log(ref.current);
-                      }          
-                    }}
-                    onChangeOrientation={(e) => console.log("changeOrientation", e)}
-                    onInit={(e) => console.log("init", e)}
-                    // renderOnlyPageLengthChanged={false}
-                    width={height * 0.7075630252100841}
-                    height={height}
-                    // width={842/1.5}
-                    // height={1190/1.5}
-                    className={""}
-                    style={{}}
-                    startPage={0}
-                    size={"fixed"}
-                    minWidth={0}
-                    minHeight={0}
-                    maxWidth={0}
-                    maxHeight={0}
-                    drawShadow={false}
-                    flippingTime={1000}
-                    usePortrait={false}
-                    startZIndex={0}
-                    autoSize={false}
-                    maxShadowOpacity={0}
-                    mobileScrollSupport={false}
-                    clickEventForward={true} // Set the forward event of clicking on child elements (buttons, links)
-                    useMouseEvents={true} // Using mouse and touch events to page flipping
-                    swipeDistance={0}
-                    showPageCorners={true} // if this value is true, fold the corners of the book when the mouse pointer is over them.
-                    disableFlipByClick={true} // if this value is true, flipping by clicking on the whole book will be locked. Only on corners
-                    showCover={false}
-                  >
-                    <div className="demoPage">
-                      <video src="./1-1.mp4" loop autoPlay={video1Ready && video2Ready} muted ref={videoRef1}
-                      onCanPlayThrough={() => setVideo1Ready(true)}
-                      ></video>
-                    </div>
-                    <div className="demoPage">
-                      <video src="./1-2.mp4"  loop autoPlay={video1Ready && video2Ready} muted ref={videoRef2}
-                      onCanPlayThrough={() => setVideo2Ready(true)}
-                      ></video>
-                    </div>
-                    <div className="demoPage">
-                      <video src="./2-1.mp4"  loop autoPlay={video3Ready && video4Ready} muted ref={videoRef3}
-                      onCanPlayThrough={() => setVideo3Ready(true)}
-                      
-                      ></video>
-                    </div>
-                    <div className="demoPage">
-                      <video src="./2-2.mp4"  loop autoPlay={video3Ready && video4Ready} muted ref={videoRef4}
-                      onCanPlayThrough={() => setVideo4Ready(true)}
-                      
-                      ></video>
-                    </div>
-                </HTMLFlipBook>
-              )}
-          </div>
-        }
+        <div className="" ref={ref}>
+          
+          
+              {isFullscreen &&
+                <div  className="flex justify-center" style={{overflow : "hidden", background : "black"}}>      
+              
+                    {width && height&& (
+                      <HTMLFlipBook
+                          onFlip={(e) => console.log("flip", e)}
+                          onUpdate={(e) => console.log("update", e)}
+                          onChangeState={(e) => {
+                            console.log("changeState", e)
+                            if(ref.current) {
+                              console.log(ref.current);
+                            }          
+                          }}
+                          onChangeOrientation={(e) => console.log("changeOrientation", e)}
+                          onInit={(e) => console.log("init", e)}
+                          // renderOnlyPageLengthChanged={false}
+                          width={height * 0.7075630252100841}
+                          height={height}
+                          // width={842/1.5}
+                          // height={1190/1.5}
+                          className={""}
+                          style={{}}
+                          startPage={0}
+                          size={"fixed"}
+                          minWidth={0}
+                          minHeight={0}
+                          maxWidth={0}
+                          maxHeight={0}
+                          drawShadow={false}
+                          flippingTime={1000}
+                          usePortrait={false}
+                          startZIndex={0}
+                          autoSize={false}
+                          maxShadowOpacity={0}
+                          mobileScrollSupport={false}
+                          clickEventForward={true} // Set the forward event of clicking on child elements (buttons, links)
+                          useMouseEvents={true} // Using mouse and touch events to page flipping
+                          swipeDistance={0}
+                          showPageCorners={true} // if this value is true, fold the corners of the book when the mouse pointer is over them.
+                          disableFlipByClick={true} // if this value is true, flipping by clicking on the whole book will be locked. Only on corners
+                          showCover={false}
+                        >
+                          <div className="demoPage">
+                            <video src="./1-1.mp4" loop autoPlay={video1Ready && video2Ready} muted ref={videoRef1}
+                            onCanPlayThrough={() => setVideo1Ready(true)}
+                            ></video>
+                          </div>
+                          <div className="demoPage">
+                            <video src="./1-2.mp4"  loop autoPlay={video1Ready && video2Ready} muted ref={videoRef2}
+                            onCanPlayThrough={() => setVideo2Ready(true)}
+                            ></video>
+                          </div>
+                          <div className="demoPage">
+                            <video src="./2-1.mp4"  loop autoPlay={video3Ready && video4Ready} muted ref={videoRef3}
+                            onCanPlayThrough={() => setVideo3Ready(true)}
+                            
+                            ></video>
+                          </div>
+                          <div className="demoPage">
+                            <video src="./2-2.mp4"  loop autoPlay={video3Ready && video4Ready} muted ref={videoRef4}
+                            onCanPlayThrough={() => setVideo4Ready(true)}
+                            
+                            ></video>
+                          </div>
+                      </HTMLFlipBook>
+                    )}
+                </div>
+              }
+          </div>            
     </>
   );
 }
